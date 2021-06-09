29f62f7d43ee47def95c184b8c07c93e6047892d
github <dagyeoms1004@gmail.com>
2021-06-09 12:41:59 +0900

    study-code

--- js/0607/script.js
+++ js/0607/script.js
@@ -22,20 +22,39 @@
 // addEventListener('scroll', function())
 
 const arrowTop = document.getElementById('arrowTop')
+const aboutSection = document.getElementById('about')
+const aboutHeight = aboutSection.clientHeight;
+const header = document.getElementById('header')
+// js 높이값 구하는 것 clientHeight
+// console.log(aboutSection.clientHeight);
+
 document.addEventListener('scroll', function(){
-    console.log('>>> scroll >>>');
-    console.log(window.scrollY)
-    if( window.scrollY > 73){
+   
+    if( window.scrollY > aboutHeight){
         arrowTop.classList.add('visible')
     }else{
         arrowTop.classList.remove('visible')
     }
+    // 스크롤이 되었을때 header가 fixed가 되어라
+    if(window.scrollY > 0){
+        header.classList.add('fixedHead');
+    }else{
+        header.classList.remove('fixedHead');
+    }
 })
 // arrow top 클릭 시 scroll이 제일 위로 올라가게
 arrowTop.addEventListener('click', function(event){
-    console.log(event)
-    // 
-    e.preventDefault();
+    // event가 발생하면 event 객체는 자동으로 전달된다.
+    // a 태그는 기본적으로 이동의 기둥을 가지고 있다.
+    // 그래서 scroll To 이전에 화면 이동이 발생해 버린다.
+    // 그러므로 preventDefault()로 막는다.
+    event.preventDefault();
     // console.log('>>> click arrow top')
     window.scrollTo({ top: 0, behavior: 'smooth' })
+});
+// navbar 클릭 시 스크롤 이동
+var navbaritem = document.querySelectorAll('.navbar__item a');
+console.log(navbarItem);
+navbaritem[0].addEventListener('click', function(event){
+    navbarItem[0].scrollIntoView({behavior: "smooth"})
 })
---
+++ js/0609/script.js
@@ -0,0 +1,9 @@
+
+$(function(){
+    var $title = $('h1');
+    var $button = $('button');
+    
+    $button.click(function(){
+        console.log('>>> click !! >>>')
+    })
+})
